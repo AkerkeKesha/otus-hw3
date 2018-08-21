6603f9d3eb669073232352665dd2e569add7b626
Akerke <akerke.kass@gmail.com>
2018-08-21 22:20:41 +0600

    wsgi compatible hello world from slides

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
 import socket
 
 
+def handle_request(server_socket):
+    client_connection, client_address = server_socket.accept()
+    raw_request = client_connection.recv(1024)
+    response = application(parse(raw_request), start_response)
+    print(response)
+    client_connection.sendall(response)
+    client_connection.close()
+
+
+def application(environ, start_response):
+    status = "HTTP/1.1 200 OK"
+    headers = [('Content-Type', 'text/plain')]
+    return start_response(status, headers)
+
+
+def start_response(status, headers):
+    response = f"{status}"
+    for key, val in headers:
+        response += "\n" + f"{key}: {val}"
+        response += "\n\n" + "Hello world!"
+    return response.encode("utf8")
+
+
+def parse(raw_request):
+    pass
+
+
 def server_forever():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('localhost', 9005))
+    server_socket.bind(('localhost', 9010))
     server_socket.listen()
     print(server_socket)
     while True:
-        client_connection, client_address = server_socket.accept()
-        request = client_connection.recv(1024)
-        print("Request: ", request)
-
-        client_connection.sendall(b"HTTP/1.1 200 OK\n\nHello!")
-        client_connection.close()
+        handle_request(server_socket)
 
 
 def main():
