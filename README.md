fda9dafa2da2aa3c66ba375d55f0baec48abaf54
Akerke <akerke.kass@gmail.com>
2018-08-26 16:46:24 +0600

    routing added to uwsgi framework

---
+++ README.md
@@ -0,0 +1,10 @@
+Homework 3
+####### (Otus, Web Development in Python)
+
+#### To set up the environment (Python version: 3.7)
+    $ virtualenv --python=$(which python3) py 
+    $ source py/bin/activate
+
+
+#### To run the program:
+    $ python3 main.py 9005 # type any available port instead of 9005
--- main.py
+++ main.py
@@ -1,12 +1,46 @@
 import socket
+import sys
+import os
 
 
-def handle_request(server_socket):
+def handle_request(server_socket, urls):
+    #TODO: html returned and never used
     client_connection, client_address = server_socket.accept()
     raw_request = client_connection.recv(1024)
-    response = application(parse(raw_request), start_response)
+    request = parse(raw_request)
+    html = route(request, urls)
+    response = application(request, start_response)
     client_connection.sendall(response)
     client_connection.close()
+
+
+def route(request, urls):
+    #TODO: view not callable
+    view = match_view(request, urls)
+    #return view(request)
+    return
+
+
+def match_view(request, urls):
+    try:
+        url = parse_url(request)
+        return urls[url]
+    except KeyError:
+        return f"Wrong url: {url}".encode('utf8')
+
+
+def parse_url(request):
+    return request['resource'].split("?")[0]
+
+
+def hello(request):
+    #TODO: call render
+    print("hello, user!")
+
+
+def render(request, template_path, template_args):
+    #TODO: complete render
+    pass
 
 
 def application(environ, start_response):
@@ -19,7 +53,6 @@ def parse(raw_request):
     d = {}
     request = bytes.decode(raw_request).split("\r\n")
     first_line = request[0].split(" ")
-
     if len(first_line) == 3:
         d['method'], d['resource'], d['version'] = first_line
     elif len(first_line) == 2:
@@ -41,23 +74,31 @@ def start_response(status, headers, content=""):
     return response.encode("utf8")
 
 
-def run():
+def run(urls):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('localhost', 9005))
+    port = int(sys.argv[1])
+    server_socket.bind(('localhost', port))
     server_socket.listen()
     while True:
-        handle_request(server_socket)
+        handle_request(server_socket, urls)
 
 
 def register(urls):
-    pass
+    dict_urls = {}
+    for url, vu in urls:
+        dict_urls[url] = vu
+    return dict_urls
 
 
-def main():
-    #register(urls)
-    run()
+def main(urls):
+    urls = register(urls)
+    run(urls)
 
 
 if __name__ == "__main__":
-    main()
+
+    BASE_DIR = os.getcwd()
+    PROJECT_NAME = 'project'
+    urls = [('/hello/', hello), ]
+    main(urls)
 
