30ce8ccb997604fc6d7086ced3fb6aa16f6e18e0
Akerke <akerke.kass@gmail.com>
2018-08-26 11:34:03 +0600

    parse request done

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ def handle_request(server_socket):
     client_connection, client_address = server_socket.accept()
     raw_request = client_connection.recv(1024)
     response = application(parse(raw_request), start_response)
-    print(response)
     client_connection.sendall(response)
     client_connection.close()
 
@@ -16,29 +15,47 @@ def application(environ, start_response):
     return start_response(status, headers)
 
 
+def parse(raw_request):
+    d = {}
+    request = bytes.decode(raw_request).split("\r\n")
+    first_line = request[0].split(" ")
+
+    if len(first_line) == 3:
+        d['method'], d['resource'], d['version'] = first_line
+    elif len(first_line) == 2:
+        d['method'], d['version'] = first_line
+
+    for pair in request[1:]:
+        if pair == '':
+            continue
+        header, value = pair.split(": ")
+        d[header.strip().lower()] = value.strip()
+    return d
+
+
 def start_response(status, headers, content=""):
     response = f"{status}"
     for key, val in headers:
         response += "\n" + f"{key}: {val}"
-        response += "\n\n" + content
+    response = response.strip() + '\r\n\r\n' + content
     return response.encode("utf8")
 
 
-def parse(raw_request):
-    pass
-
-
-def server_forever():
+def run():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('localhost', 9010))
+    server_socket.bind(('localhost', 9005))
     server_socket.listen()
-    print(server_socket)
     while True:
         handle_request(server_socket)
 
 
+def register(urls):
+    pass
+
+
 def main():
-    server_forever()
+    #register(urls)
+    run()
 
 
 if __name__ == "__main__":
